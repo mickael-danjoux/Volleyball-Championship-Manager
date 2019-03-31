e6f84c374834625f3a5de5dc05464e5a2593b94f
Fgimaret <florent.gimaret@gmail.com>
2019-03-31 20:58:13 +0200

    - Suppression / Recherche d'un club

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -22,29 +22,47 @@ class ClubController extends AbstractController
                 ->getRepository(Club::class)
                 ->findAll();
 
-            return $this->render('club/club.list.html.twig', ['clubs' => $clubs], new Response("Affichage des clubs OK", 200));
+            return $this->render('club/club.list.html.twig', ['clubs' => $clubs], new Response(200));
+
+        } else if ($req->isMethod('POST')){
+
+            $clubName = $req->get("searchClub");
+            $clubs = $this->getDoctrine()
+                ->getRepository(Club::class)
+                    ->findBy(array('name' => $clubName));
+
+            return $this->render('club/club.list.html.twig', ['clubs' => $clubs], new Response(200));
         }
+
+        return $this->redirect($req->getUri());
     }
 
     /**
      * @Route("/club/form", name="club-form")
      */
-    public function club(Request $req) {
+    public function clubForm(Request $req) {
 
         if ($req->isMethod('GET')){
 
-            $id = $req->get('id');
-            $club = $this->getDoctrine()->getRepository(Club::class)->find($id);
+            if ($req->get('id') != null) {
 
-            return $this->render('club/club.form.html.twig', [
-                'id' => $id,
-                'name' => $club->getName(),
-                'managerLastName' => $club->getManagerLastName(),
-                'managerFirstName' => $club->getManagerFirstName(),
-                'email' => $club->getEmail(),
-                'phoneNumber' => $club->getPhoneNumber(),
-                'active' => $club->getActive()
-            ], new Response(200));
+                $id = $req->get('id');
+                $club = $this->getDoctrine()->getRepository(Club::class)->find($id);
+
+                return $this->render('club/club.form.html.twig', [
+                    'id' => $id,
+                    'name' => $club->getName(),
+                    'managerLastName' => $club->getManagerLastName(),
+                    'managerFirstName' => $club->getManagerFirstName(),
+                    'email' => $club->getEmail(),
+                    'phoneNumber' => $club->getPhoneNumber(),
+                    'active' => $club->getActive()
+                ], new Response(200));
+
+            } else {
+
+                return $this->render('club/club.form.html.twig', [], new Response(200));
+            }
 
         } else if ($req->isMethod('POST')) {
 
@@ -61,19 +79,29 @@ class ClubController extends AbstractController
 
             $club = $this->getDoctrine()->getRepository(Club::class)->find($id);
             if($club !== null) {
-                $club->setAccount($name, $managerLastName, $managerFirstName, $email, $password, $phoneNumber, $active);
+                $club->setAccount($name, $managerLastName, $managerFirstName, $email, $phoneNumber, $active);
             } else {
                 $club = new Club($email, $password, $phoneNumber, $name, $managerFirstName, $managerLastName, $active);
             }
 
             $entityManager->persist($club);
-
             $entityManager->flush();
 
-            return $this->render('club/club.form.html.twig', [], new Response(200));
-            //return new Response($req->get("active"));
+            return $this->redirectToRoute('club');
 
+        } else if ($req->isMethod('DELETE')) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $id = $req->get("id");
+            $club = $this->getDoctrine()->getRepository(Club::class)->find($id);
+
+            $entityManager->remove($club);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('club');
         }
+        return null;
     }
 
 }
--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -67,12 +67,11 @@ class Account
         $this->active = $active;
     }
 
-    public function setAccount($name, $managerLastName, $managerFirstName, $email, $password, $phoneNumber, $active): void {
+    public function setAccount($name, $managerLastName, $managerFirstName, $email, $phoneNumber, $active): void {
         $this->name = $name;
         $this->managerLastName = $managerLastName;
         $this->managerFirstName = $managerFirstName;
         $this->email = $email;
-        $this->password = $password;
         $this->phoneNumber = $phoneNumber;
         $this->active = $active;
     }
