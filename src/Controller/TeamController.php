0e5bd0b26084de4954da32cf66d8023c98447503
DANJOUX MICKAEL p1602272 <mickael.danjoux@etu.univ-lyon1.fr>
2019-04-01 10:28:58 +0200

    team

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Team;
+use App\Entity\Club;
+
 class TeamController extends AbstractController
 {
 
@@ -15,21 +18,25 @@ class TeamController extends AbstractController
      * @Route("team/form",name="team")
      */
     public function addTeam(Request $req){
-        if($req->isMethod("GET")){
-            return $this->render("team/team.form.html.twig");
-        }
-        elseif ($req->isMethod("POST")){
+
+        if ($req->isMethod("POST")){
             $entityManager = $this->getDoctrine()->getManager();
+            $clubId = $req->get("clubId");
+            $club = $this->getDoctrine()->getRepository(Club::class)->find($clubId);
+
             $lastName = $req->get("inputCaptainLastName");
-            $firstName = $req->get("inputCaptainfirstName");
+            $firstName = $req->get("inputCaptainFirstName");
             $mail = $req->get("inputCaptainMail");
             $phoneNumber = $req->get("inputCaptainPhoneNumber");
             $volleyballCourtId = $req->get("selectVolleyballCourt");
 
-            $account = new Account($mail, "", $phoneNumber, "nameTeam", $firstName, $lastName, $volleyballCourtId);
-            $entityManager->persist($account);
+            $team = new Team(1, $club, $mail, "", $phoneNumber, "TeamName", $firstName, $lastName, 1);
+            $entityManager->persist($team);
             $entityManager->flush();
-            return $this->render('club/club.form.html.twig');
+            return $this->render('team/team.form.html.twig');
         }
+
+        $clubId = $req->get('id');
+        return $this->render("team/team.form.html.twig",['clubId' => $clubId]);
     }
 }
