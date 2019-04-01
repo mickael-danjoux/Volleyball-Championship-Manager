d3a32ef4d6c8abb36e5fb5dad509bd4eb1d0c227
Fgimaret <florent.gimaret@gmail.com>
2019-04-01 11:13:04 +0200

    - Liste équipes + ajout

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -1,10 +1,7 @@
 <?php
-<<<<<<< HEAD
-
 
 namespace App\Controller;
 
-=======
 /**
  * Created by PhpStorm.
  * User: flore
@@ -17,18 +14,35 @@ namespace App\Controller;
 
 use App\Entity\Club;
 use App\Entity\Team;
->>>>>>> e1523c65dc42bb1fcbb228973bbba032e79f88f9
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-<<<<<<< HEAD
-use App\Entity\Team;
-use App\Entity\Club;
-
 class TeamController extends AbstractController
 {
+    /**
+     * @Route("/club/team", name="club-team")
+     */
+    public function getTeams(Request $req) {
+
+        if ($req->isMethod('GET')) {
+
+            $clubId = $req->get("clubId");
+
+            $club = $this->getDoctrine()
+                ->getRepository(Club::class)
+                ->find($clubId);
+
+            $teams = $this->getDoctrine()
+                ->getRepository(Team::class)
+                ->findBy(array('club' => $club));
+
+            return $this->render('team/team.list.html.twig', ['teams' => $teams, 'clubId' => $clubId], new Response(200));
+        }
+
+        return null;
+    }
 
     /**
      * @Route("team/form",name="team")
@@ -52,59 +66,7 @@ class TeamController extends AbstractController
             return $this->render('team/team.form.html.twig');
         }
 
-        $clubId = $req->get('id');
+        $clubId = $req->get('clubId');
         return $this->render("team/team.form.html.twig",['clubId' => $clubId]);
-=======
-class TeamController extends AbstractController
-{
-    /**
-     * @Route("/club/team", name="club-team")
-     */
-    public function getTeams(Request $req) {
-
-        if ($req->isMethod('GET')) {
-            $teams = $this->getDoctrine()
-                ->getRepository(Team::class)
-                ->findAll();
-
-            $clubId = $req->get("clubId");
-
-            return $this->render('team/team.list.html.twig', ['teams' => $teams, "clubId" => $clubId], new Response(200));
-        }
-
-        return null;
-    }
-
-    /**
-     * @Route("/club/team/form", name="team")
-     */
-    public function teamForm(Request $req)
-    {
-
-        if ($req->isMethod('GET')) {
-
-            if ($req->get('id') != null) {
-
-                $id = $req->get('id');
-                $club = $this->getDoctrine()->getRepository(Club::class)->find($id);
-
-                return $this->render('team/team.form.html.twig', [
-                    'id' => $id,
-                    'name' => $club->getName(),
-                    'managerLastName' => $club->getManagerLastName(),
-                    'managerFirstName' => $club->getManagerFirstName(),
-                    'email' => $club->getEmail(),
-                    'phoneNumber' => $club->getPhoneNumber(),
-                    'active' => $club->getActive()
-                ], new Response(200));
-
-            } else {
-
-                return $this->render('team/team.form.html.twig', [], new Response(200));
-            }
-
-        }
-        return null;
->>>>>>> e1523c65dc42bb1fcbb228973bbba032e79f88f9
     }
 }
